34eabc635d4a29cba7da84c8c4471fccac47d561
BenhammadiFatimaZahrae <benhammadifatimazahrea2084@gmail.com>
2026-04-22 13:16:08 +0100

    version 2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Version 2 - BEN HHHHAMMADI FATIMA11"
+    return "Version 2 - Déployée automatiquement EN HAMMADI FATIMA ZAHRAE"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
